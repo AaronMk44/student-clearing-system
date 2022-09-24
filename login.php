5b6b973b2e4cb0588268aa5d65267ac969afc85e
Mike Mambwe <88389755+aeprodigy@users.noreply.github.com>
2022-09-24 10:36:17 +0200

    Update login.php File

--- login.php
+++ login.php
@@ -7,10 +7,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
   <title>Log In - Student Clearing System</title>
+  <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-
+  <div class="title"><h1>Log In </h1></div>
+  <div class="container">
+    <div class="left"><img src="assets/img/logo.png" alt=""></div>
+    <div class="right">
+      
+      <div class="formBox">
+        <p>Please enter your details</p>
+        <form action="" method="POST">
+          <p></p>
+          <input type="text" placeholder="User Name" >
+          <p></p>
+          <input type="password" placeholder="passowrd">
+          <input type="submit" name="" value="Log in">
+          <a href="#">Forgot Password?</a>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
