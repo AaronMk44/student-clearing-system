08053474cc72de622b5a4dc99653c1a8c32f8f9c
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-09-25 09:39:53 +0200

    feat: add model to handle db interactions to admin table

---
+++ models/AdminModel.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once 'Database.php';
+
+class AdminModel
+{
+  private Database $db;
+  private string $TABLE = 'administrators';
+
+  public function __construct()
+  {
+    $this->db = new Database();
+  }
+
+  public function add(Admin $a): bool
+  {
+    $hashedPasssword = password_hash($a->password, PASSWORD_BCRYPT);
+
+    $sql =
+      "insert into $this->TABLE 
+      (
+        first_name,last_name,gender,
+        admin_role,email,password
+      ) 
+      values 
+      (
+        '$a->firstName', '$a->lastName', '$a->gender'
+        '$a->adminRole','$a->email', '$hashedPasssword'
+      )";
+
+    try {
+      $this->db->connect()->query($sql);
+      return true;
+    } catch (\Throwable $th) {
+      return false;
+    }
+  }
+
+  public function find(string $email): Admin | null
+  {
+    $sql = "select * from $this->TABLE where email = '$email'";
+    $result = $this->db->connect()->query($sql);
+
+    $a = new Admin();
+
+    foreach ($result as $r) {
+      $a->adminID = $r['admin_id'];
+      $a->firstName = $r['first_name'];
+      $a->lastName = $r['last_name'];
+      $a->gender = $r['gender'];
+      $a->adminRole = $r['admin_role'];
+      $a->email = $r['email'];
+      $a->password = $r['password'];
+    }
+
+    return $a;
+  }
+
+  public function edit(Admin $a): bool
+  {
+    $sql = "
+    update $this->TABLE 
+    set 
+      first_name = '$a->firstName',
+      last_name = '$a->lastName',
+      gender = '$a->gender',
+      admin_role = '$a->adminRole',
+      email = '$a->email'
+    where admin_id = '$a->adminID' ";
+
+    try {
+      $this->db->connect()->query($sql);
+      return true;
+    } catch (\Throwable $th) {
+      return false;
+    }
+  }
+}
