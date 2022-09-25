799354cf1b525c48708c73a4616aa86a6f858b30
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-09-25 09:37:07 +0200

    update: add basic routing and authentication

--- index.php
+++ index.php
@@ -7,4 +7,13 @@ $is_logged_in = $_SESSION['is_logged_in'];
 
 if ($is_logged_in === null || $is_logged_in === false) {
   header('location: login.php');
+} else {
+  switch ($_SESSION['user_type']) {
+    case 'student':
+      header('location: student/dashboard.php');
+      break;
+
+    default:
+      break;
+  }
 }
