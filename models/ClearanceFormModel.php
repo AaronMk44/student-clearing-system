54f665ab2d59b5c6c8511819210e0833d589f85f
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-09-27 13:53:19 +0200

    update: add methods to fetch pending and approved methods

--- models/ClearanceFormModel.php
+++ models/ClearanceFormModel.php
@@ -84,4 +84,103 @@ class ClearanceFormModel
 
     return $forms;
   }
+
+  public function getPendingFormsFor(int $studentID): array
+  {
+    $sql = "
+    select * from $this->TABLE 
+    where 
+    student_id = $studentID and 
+    (hod_approval_status != 'approved' or 
+    hostel_rep_approval_status != 'approved' or 
+    hostel_rep_approval_status != 'approved' or 
+    librarian_approval_status != 'approved')";
+    $result = $this->db->connect()->query($sql);
+
+    $forms = [];
+
+    foreach ($result as $r) {
+      $f = new ClearanceForm();
+
+      $f->formID = $r['form_id'];
+      $f->studentID = $r['student_id'];
+      $f->studentName = $r['student_name'];
+      $f->yearOfStudy = $r['student_year_of_study'];
+      $f->roomNo = $r['student_room_no'];
+      $f->term = $r['clear_for_term'];
+
+      $f->hodName = $r['hod_name'];
+      $f->hodApprovalStatus = $r['hod_approval_status'];
+      $f->hodApprovalDate = $r['hod_approval_date'];
+
+      $f->hostelRepName = $r['hostel_rep_name'];
+      $f->hsotelRepApprovalStatus = $r['hostel_rep_approval_status'];
+      $f->hostelItemsDue = $r['hostel_items_due'];
+      $f->hostelRepApprovalDate = $r['hostel_rep_approval_date'];
+
+      $f->librarianName = $r['librarian_name'];
+      $f->librarianApprovalStatus = $r['librarian_approval_status'];
+      $f->libraryItemsDue = $r['library_items_due'];
+      $f->librarianApprovalDate = $r['librarian_approval_date'];
+
+      $f->accountantName = $r['accountant_name'];
+      $f->accountantApprovalStatus = 'accountant_approval_status';
+      $f->accountsRunningBalance = $r['student_running_balance'];
+      $f->accountantApprovalDate = $r['accountant_approval_date'];
+
+      $forms[] = $f;
+    }
+
+    return $forms;
+  }
+
+  public function getApprovedFormsFor(int $studentID): array
+  {
+    $sql = "
+    select * from $this->TABLE 
+    where 
+    student_id = $studentID and 
+    hod_approval_status = 'approved' and 
+    hostel_rep_approval_status = 'approved' and 
+    hostel_rep_approval_status = 'approved' and 
+    librarian_approval_status = 'approved'";
+
+    $result = $this->db->connect()->query($sql);
+
+    $forms = [];
+
+    foreach ($result as $r) {
+      $f = new ClearanceForm();
+
+      $f->formID = $r['form_id'];
+      $f->studentID = $r['student_id'];
+      $f->studentName = $r['student_name'];
+      $f->yearOfStudy = $r['student_year_of_study'];
+      $f->roomNo = $r['student_room_no'];
+      $f->term = $r['clear_for_term'];
+
+      $f->hodName = $r['hod_name'];
+      $f->hodApprovalStatus = $r['hod_approval_status'];
+      $f->hodApprovalDate = $r['hod_approval_date'];
+
+      $f->hostelRepName = $r['hostel_rep_name'];
+      $f->hsotelRepApprovalStatus = $r['hostel_rep_approval_status'];
+      $f->hostelItemsDue = $r['hostel_items_due'];
+      $f->hostelRepApprovalDate = $r['hostel_rep_approval_date'];
+
+      $f->librarianName = $r['librarian_name'];
+      $f->librarianApprovalStatus = $r['librarian_approval_status'];
+      $f->libraryItemsDue = $r['library_items_due'];
+      $f->librarianApprovalDate = $r['librarian_approval_date'];
+
+      $f->accountantName = $r['accountant_name'];
+      $f->accountantApprovalStatus = 'accountant_approval_status';
+      $f->accountsRunningBalance = $r['student_running_balance'];
+      $f->accountantApprovalDate = $r['accountant_approval_date'];
+
+      $forms[] = $f;
+    }
+
+    return $forms;
+  }
 }
