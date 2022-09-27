36cce4c73fa8b92e74e0547936ca55a5540e1afb
Aaron Mkandawire <aaronmk2001@gmail.com>
2022-09-27 13:30:23 +0200

    update: unset session variables

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 
 session_start();
+$_SESSION['is_logged_in'] = false;
+$_SESSION['user_type'] = null;
+$_SESSION['user_email'] = '';
 session_unset();
 session_destroy();
 header('location: login.php');
